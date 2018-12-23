77fa36b5359176e57bd5d14e33b162e58450a391
amoewdy <amoewdy@gmail.com>
2018-12-23 02:02:00 -0600

    another personal website

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="nav">
+        <div class="nav-title" onclick="window.location.href='/'" style="cursor: pointer;"><img src="./assets/muben logo 2.png" width="6%"> Sixuan Li</div>
+        <ul>
+            <li class="nav-title" onclick="window.location.href='/'" ><img src="/images/index/muben logo 2.png" width="6%" > Sixuan Li</li>
+            <li onclick="window.location.href='/'">Work</li>
+            <li onclick="window.location.href='/about'">Resume</li>
+            <li onclick="window.location.href='/about'">About</li>
+        </ul>
+    </div>
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
