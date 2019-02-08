efede0fc36c9909cba8778334514fd7613830067
amoewdy <amoewdy@gmail.com>
2019-02-08 01:24:36 -0800

    update

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -108,11 +108,11 @@
 export default {
     name: 'image',
     data () {
-    return {
-      seen:false,  
-      imgSrc: '../static/home/snowBackgroundFigma.png'
-    }
-  },
+        return {
+        seen:false,  
+        imgSrc: '../static/home/snowBackgroundFigma.png'
+        }
+    },
     mounted: function() {
         this.snow();
     },
@@ -124,125 +124,126 @@ export default {
             this.seen = false;
         },
         snow:function(){
-(function() {
-    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
-    function(callback) {
-        window.setTimeout(callback, 1000 / 60);
-    };
-    window.requestAnimationFrame = requestAnimationFrame;
-})();
+            // (function() {
+            //     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
+            //     function(callback) {
+            //         window.setTimeout(callback, 1000 / 60);
+            //     };
+            //     window.requestAnimationFrame = requestAnimationFrame;
+            // })();
 
-var flakes = [],
-    canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext("2d"),
-    flakeCount = 400,
-    mX = -100,
-    mY = -100
-    // canvas.width = window.innerWidth;
-    // canvas.height = window.innerHeight;
-    canvas.width = 450;
-    canvas.height =450;
+            var flakes = [],
+                canvas = document.getElementById("canvas"),
+                ctx = canvas.getContext("2d"),
+                flakeCount = 400,
+                mX = -100,
+                mY = -100
+                // canvas.width = window.innerWidth;
+                // canvas.height = window.innerHeight;
+                canvas.width = 450;
+                canvas.height =450;
 
-function snow() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+            function snow2() {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (var i = 0; i < flakeCount; i++) {
-        var flake = flakes[i],
-            x = mX,
-            y = mY,
-            minDist = 150,
-            x2 = flake.x,
-            y2 = flake.y;
+                for (var i = 0; i < flakeCount; i++) {
+                    var flake = flakes[i],
+                        x = mX,
+                        y = mY,
+                        minDist = 150,
+                        x2 = flake.x,
+                        y2 = flake.y;
 
-        var dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
-            dx = x2 - x,
-            dy = y2 - y;
+                    var dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
+                        dx = x2 - x,
+                        dy = y2 - y;
 
-        if (dist < minDist) {
-            var force = minDist / (dist * dist),
-                xcomp = (x - x2) / dist,
-                ycomp = (y - y2) / dist,
-                deltaV = force / 2;
+                    if (dist < minDist) {
+                        var force = minDist / (dist * dist),
+                            xcomp = (x - x2) / dist,
+                            ycomp = (y - y2) / dist,
+                            deltaV = force / 2;
 
-            flake.velX -= deltaV * xcomp;
-            flake.velY -= deltaV * ycomp;
+                        flake.velX -= deltaV * xcomp;
+                        flake.velY -= deltaV * ycomp;
 
-        } else {
-            flake.velX *= .98;
-            if (flake.velY <= flake.speed) {
-                flake.velY = flake.speed
+                    } else {
+                        flake.velX *= .98;
+                        if (flake.velY <= flake.speed) {
+                            flake.velY = flake.speed
+                        }
+                        flake.velX += Math.cos(flake.step += .05) * flake.stepSize;
+                    }
+                    ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
+                    flake.y += flake.velY;
+                    flake.x += flake.velX;
+                        
+                    if (flake.y >= canvas.height || flake.y <= 0) {
+                        reset(flake);
+                    }
+
+
+                    if (flake.x >= canvas.width || flake.x <= 0) {
+                        reset(flake);
+                    }
+
+                    ctx.beginPath();
+                    ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
+                    ctx.fill();
+                }
+                requestAnimationFrame(snow2);
+            };
+
+            function reset(flake) {
+                flake.x = Math.floor(Math.random() * canvas.width);
+                flake.y = 0;
+                flake.size = (Math.random() * 3) + 1.8;
+                flake.speed = (Math.random() * 1) + 0.5;
+                flake.velY = flake.speed;
+                flake.velX = 0;
+                flake.opacity = (Math.random() * 0.5) + 0.5;
             }
-            flake.velX += Math.cos(flake.step += .05) * flake.stepSize;
-        }
-        ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
-        flake.y += flake.velY;
-        flake.x += flake.velX;
+
+            function init() {
+                for (var i = 0; i < flakeCount; i++) {
+                    var x = Math.floor(Math.random() * canvas.width),
+                        y = Math.floor(Math.random() * canvas.height),
+                        size = (Math.random() * 3) + 1.7,
+                        speed = (Math.random() * 1) + 0.5,
+                        opacity = (Math.random() * 0.5) + 0.4;
+
+                    flakes.push({
+                        speed: speed,
+                        velY: speed,
+                        velX: 0,
+                        x: x,
+                        y: y,
+                        size: size,
+                        stepSize: (Math.random()) / 30,
+                        step: 0,
+                        opacity: opacity
+                    });
+                }
+            }
+
             
-        if (flake.y >= canvas.height || flake.y <= 0) {
-            reset(flake);
-        }
 
+            canvas.addEventListener("mousemove", function(e) {
+                let rect = canvas.getBoundingClientRect();
+                mX = e.clientX;
+                mY = e.clientY;
+                mX = mX - rect.left * (canvas.width/rect.width);
+                mY = mY - rect.top * (canvas.height/rect.height);
+            });
 
-        if (flake.x >= canvas.width || flake.x <= 0) {
-            reset(flake);
-        }
-
-        ctx.beginPath();
-        ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
-        ctx.fill();
-    }
-    requestAnimationFrame(snow);
-};
-
-function reset(flake) {
-    flake.x = Math.floor(Math.random() * canvas.width);
-    flake.y = 0;
-    flake.size = (Math.random() * 3) + 1.8;
-    flake.speed = (Math.random() * 1) + 0.5;
-    flake.velY = flake.speed;
-    flake.velX = 0;
-    flake.opacity = (Math.random() * 0.5) + 0.5;
-}
-
-function init() {
-    for (var i = 0; i < flakeCount; i++) {
-        var x = Math.floor(Math.random() * canvas.width),
-            y = Math.floor(Math.random() * canvas.height),
-            size = (Math.random() * 3) + 1.7,
-            speed = (Math.random() * 1) + 0.5,
-            opacity = (Math.random() * 0.5) + 0.4;
-
-        flakes.push({
-            speed: speed,
-            velY: speed,
-            velX: 0,
-            x: x,
-            y: y,
-            size: size,
-            stepSize: (Math.random()) / 30,
-            step: 0,
-            opacity: opacity
-        });
-    }
-
-    snow();
-};
-
-canvas.addEventListener("mousemove", function(e) {
-    let rect = canvas.getBoundingClientRect();
-    mX = e.clientX;
-    mY = e.clientY;
-    mX = mX - rect.left * (canvas.width/rect.width);
-    mY = mY - rect.top * (canvas.height/rect.height);
-});
-
-window.addEventListener("resize",function(){
-    // canvas.width = window.innerWidth;
-    // canvas.height = window.innerHeight;
-    canvas.width = 450;
-    canvas.height = 450;
-})
-init();
+            window.addEventListener("resize",function(){
+                // canvas.width = window.innerWidth;
+                // canvas.height = window.innerHeight;
+                canvas.width = 450;
+                canvas.height = 450;
+            })
+            init();
+            snow2();
         }
     }
 }
